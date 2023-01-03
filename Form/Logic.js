199d4330718e39bdb1450d3e3fb5feb0eb24604a
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2023-01-03 21:43:29 +0530

    Added: Enter valid email from user

--- Form/Logic.js
+++ Form/Logic.js
@@ -6,3 +6,13 @@ const text = document.querySelector("#text");
         if (nameRegex.test(text.value)) textError.textContent = " ";
         else textError.textContent = "Name is Incorrect";
       });
+
+      const email = document.querySelector('#email');
+      const emailError = document.querySelector('.email-error');
+      email.addEventListener('input', function input(){
+          let emailRegex = RegExp('^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@([a-z1-9]+)([.][a-z]*)?(\.[a-z]{2,})$');
+          if(emailRegex.test(email.value)) 
+              emailError.textContent = " ";
+          else emailError.textContent = "Email is Incorrect";
+      });
+  
